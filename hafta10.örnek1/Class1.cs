c3b0f6e47fdbd7a6311ce4ec5054b4a244160741
yusufbagci17 <151386608+yusufbagci17@users.noreply.github.com>
2024-12-02 22:22:53 +0300

    Add files via upload

---
+++ hafta10.örnek1/Class1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hafta10.örnek1
+{
+    public class Calisan
+    {
+        public static string SirketAdi{ get; set; }
+        public static int CalisanSayisi {  get; set; }
+        
+        public int Maas {  get; set; }  
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+        static Calisan()
+        {
+            CalisanSayisi = 0; // İlk başta çalışan sayısı sıfır
+            Console.WriteLine("Şirket bilgileri yüklendi.");
+        }
+        public Calisan(string ad, string soyad ,int maas)
+        {
+            Ad = ad;
+            Maas = maas;
+            Soyad = soyad;
+            CalisanSayisi++;
+        }
+       
+
+        public static void SirketBilgileriniYazdir()
+        {
+            Console.WriteLine($"sirket adı :{SirketAdi}");
+            Console.WriteLine($"çalışan sayisi:{CalisanSayisi}");
+            
+        }
+        public void CalisanBilgisiYazdir()
+        {
+            Console.WriteLine($"Ad: {Ad}, Soyad: {Soyad}, Maaş: {Maas}");
+        }
+    }
+}
---
+++ hafta10.örnek1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hafta10.örnek1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Statik metodu kullanarak şirket bilgilerini yazdıralım
+            Console.WriteLine("Şirket bilgileri:");
+            Calisan.SirketBilgileriniYazdir();
+
+            // Çalışan bilgilerini eklemek ve yazdırmak
+            Console.WriteLine("\nÇalışan bilgileri:");
+            Calisan calisan1 = new Calisan("Ahmet", "Yılmaz", 5000);
+            calisan1.CalisanBilgisiYazdir();
+
+            Calisan calisan2 = new Calisan("Zeynep", "Demir", 6000);
+            calisan2.CalisanBilgisiYazdir();
+
+            // Güncellenmiş şirket bilgilerini kontrol edelim
+            Console.WriteLine("\nGüncellenmiş şirket bilgileri:");
+            Calisan.SirketBilgileriniYazdir();
+
+            Console.ReadLine();
+        }
+    }
+}
